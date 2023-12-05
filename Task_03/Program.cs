7cf830d55d26dc7e00d76560448e1dd4440c4124
Pavel <shadow-hunter2000@yandex.ru>
2023-12-06 00:53:23 +0800

    Задача на массив и строки

---
+++ Task_03/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string stroka = "Hello";
+bool Palindrom = IsPalindrome(stroka);
+
+
+
+bool IsPalindrome(string str)
+{
+    for (int i = 0; i < str.Length/2; i++)
+    {
+        if(str[i] != str[str.Length -1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+if(Palindrom) 
+{
+    System.Console.WriteLine($" Строка {stroka} является палиндромом ");
+}
+else
+{
+    System.Console.WriteLine($" Строка {stroka} не является палиндромом ");
+}
+
+
+
---
+++ Tssk_02/Program.cs
@@ -0,0 +1,15 @@
+﻿string stroka = "Hello";
+char[] chararray = stroka.ToCharArray();
+for (int i = 0; i < chararray.Length; i++)
+{
+    if (char.IsUpper(chararray[i]))
+        {  
+            chararray[i] = (char)(chararray[i] + ('a' - 'A'));
+        }
+    }
+    string result = new string(chararray);
+
+    Console.WriteLine(result);
+
+
+
